ea58c4e1474afa7995bc48cbf69fc902beaad2b7
Valeriia Muradian <valeriiamuradian@Valeriias-MacBook-Air.local>
2018-11-16 19:23:53 -0800

    drawing lineslol lo

--- test.c
+++ test.c
@@ -13,32 +13,27 @@ int key_press(int key, void *param)
 	return (0);
 }
 
-static void drawline(int x0, int y0, int x1, int y1)
+void drawline(int x0, int y0, int x1, int y1, void *mlx_ptr, void *win_ptr)
 {
-	void *mlx_ptr = mlx_init();
-	void *win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Some shit");
-	int dx, dy, f, x, y;
+	int dx, dy, f;
 
-	dx = x1-x0;
-	dy = y1-y0;
-	x = x0;
-	y = y0;
-	f = 2*dy-dx;
+	dx = x1 - x0;
+	dy = y1 - y0;
+	f = dx / 2;
 
-	while (x < x1)
+	while (x0 < x1)
 	{
-		if (f >= 0)
+		mlx_pixel_put(mlx_ptr, win_ptr, x0, y0, 0xffd700);
+		if (f < 0)
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x0000ff);
-			y=y+1;
-			f=f+2*dy-2*dx;
+			f = f + dx;
 		}
 		else
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x0000ff);
-			f = f+2*dy;
+			f = f - dy;
+			y0++;
 		}
-		x = x+1;
+		x0++;
 	}
 }
 
@@ -50,10 +45,9 @@ int	main(void)
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Some shit");
-	//mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0x0000ff);
-	//mlx_key_hook (win_ptr, key_press, (void *)0);
-	printf("Enter co-ordinates of first point and second: ");
-	scanf("%d%d%D%D", &x0, &y0, &x1, &y1);
-    drawline(x0, y0, x1, y1);
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffd700);
+//	mlx_string_put(mlx_ptr, win_ptr, 200, 300, 0xffd700, "Hello");
+	drawline(100, 100, 200, 150, mlx_ptr, win_ptr);
+	mlx_key_hook (win_ptr, key_press, (void *)0);
 	mlx_loop(mlx_ptr);
 }
