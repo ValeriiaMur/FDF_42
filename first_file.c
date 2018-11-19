80fcd449384f08ebe8a6cd8ec760bd7aa3427c26
Valeriia Muradian <valeriiamuradian@Valeriias-MacBook-Air.local>
2018-11-19 11:26:48 -0800

    some funcs

---
+++ first_file.c
@@ -0,0 +1,17 @@
+#include "mlx.h"
+#include <stdio.h>
+#include "libft.h"
+#include <math.h>
+
+typedef struct  s_map
+{
+  double  x;
+  double  y;
+  double  z;
+  int     color;
+}               t_map;
+
+int main(int argc, char **argv)
+{
+
+}
--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include "./minilibx/mlx.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 void ft_putchar(char c)
 {
@@ -20,7 +22,6 @@ void drawline(int x0, int y0, int x1, int y1, void *mlx_ptr, void *win_ptr)
 	dx = x1 - x0;
 	dy = y1 - y0;
 	f = dx / 2;
-
 	while (x0 < x1)
 	{
 		mlx_pixel_put(mlx_ptr, win_ptr, x0, y0, 0xffd700);
@@ -37,6 +38,24 @@ void drawline(int x0, int y0, int x1, int y1, void *mlx_ptr, void *win_ptr)
 	}
 }
 
+int *exit_x(void *param)
+{
+	param = NULL;
+	exit(0);
+	return (0);
+}
+
+static void iso(int *x, int *y, int z)
+{
+    int previous_x;
+    int previous_y;
+
+    previous_x = *x;
+    previous_y = *y;
+    *x = (previous_x - previous_y) * cos(0.523599);
+    *y = -z + (previous_x + previous_y) * sin(0.523599);
+}
+
 int	main(void)
 {
 	int x0, y0, x1, y1, dx, dy;
@@ -44,10 +63,12 @@ int	main(void)
 	void *win_ptr;
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Some shit");
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffd700);
-//	mlx_string_put(mlx_ptr, win_ptr, 200, 300, 0xffd700, "Hello");
-	drawline(100, 100, 200, 150, mlx_ptr, win_ptr);
-	mlx_key_hook (win_ptr, key_press, (void *)0);
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Some Shit");
+	//mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffd700);
+//	mlx_string_put(mlx_ptr, win_ptr, 100, 450, 0xffd700, "Fuck you");
+	drawline(100, 100, 180, 200, mlx_ptr, win_ptr);
+//	mlx_key_hook (win_ptr, key_press, (void *)0);
 	mlx_loop(mlx_ptr);
+	iso (&100, &100, 100);
+	mlx_hook(win_ptr, 17, 1L<<17, exit_x(0), 0);
 }
